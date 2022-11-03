736eea1fbc8ec7c025066970fbca537c13391153
saraswathy <saraswathychandra88@gmail.com>
2022-11-03 07:26:52 +0100

    angular project upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ @ViewChild('myForm')form:NgForm;
   title = 'RegisterForm';
+  defaultCountry="2020";
+  fullName:string="";
+  email:string="";
+  defaultSubscribe="yes";
+  defaultCreateApp="yes";
+  createApp=[{id:"1",value:"yes"},
+  {id:"2",value:"No"},{id:"3",value:"Maybe"}];
+  subscribe=[{id:"1",value:"yes"},
+  {id:"2",value:"No"}];
+  submit(){
+    console.log(this.form)
+    this.form.reset();
+    alert("your Request is submitted ")
+  }
+  onSubmit(register){
+    console.log(register);
+    this.http.post('https://registrationform-aed6f-default-rtdb.firebaseio.com/register.json',register)
+    .subscribe((res)=>console.log(res));
+  }
+  setvalue(){
+    this.form.setValue({
+      DedicatedApp: "",
+course: "",
+email: "sai@gmail.com",
+fullName: "sai sarvesh",
+year: "",
+yesForSub: ""
+    })
+  }
+
+  constructor(private http:HttpClient){
+   
+
+  }
 }
